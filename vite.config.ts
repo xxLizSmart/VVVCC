0ce0162c5aa5a3e0bd0b75e38316c93f5e987be9
xxLizSmart <lizsmartxx@gmail.com>
2026-01-30 20:57:47 +0800

    Updated package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import runtimeErrorModal from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
   plugins: [
     react(),
+    runtimeErrorModal(),
   ],
   resolve: {
     alias: {
